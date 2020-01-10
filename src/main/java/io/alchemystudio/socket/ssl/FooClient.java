2d0d8f09673b863655978f6bdd0cbfaeb72e4ab4
阿男 <l.weinan@gmail.com>
2020-01-10 17:27:29 +0800

    add FooClient

---
+++ src/main/java/io/alchemystudio/socket/ssl/FooClient.java
@@ -0,0 +1,16 @@
+package io.alchemystudio.socket.ssl;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+
+public class FooClient {
+    public static void main(String[] args) throws Exception {
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        HttpResponse response = client.execute(new HttpGet("http://as1.io"));
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("::: " + statusCode);
+    }
+}
